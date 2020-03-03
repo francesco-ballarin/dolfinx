22e56236d0368e9928dc45df8dcecc46ba4ab3a7
Francesco Ballarin <francesco.ballarin@sissa.it>
2020-03-03 13:47:49 +0100

    Add new VecSubVectorReadWrapper and VecSubVectorWrapper classes to la utils, to be used in combination with DofMapRestriction in the python layer

--- cpp/dolfinx/la/petsc.h
+++ cpp/dolfinx/la/petsc.h
@@ -88,6 +88,13 @@ Vec create_vector_wrap(const la::Vector<V>& x)
   return create_vector_wrap(*x.index_map(), x.bs(), x.array());
 }
 
+/// Ghost block layout types
+enum class GhostBlockLayout
+{
+  intertwined,  // [owned_0, ghost_0, owned_1, ghost_1, ..., ...], as used by block matrices
+  trailing      // [owned_0, owned_1, ..., ghost_0, ghost_1, ...], as used by block vectors
+};
+
 /// @brief Compute PETSc IndexSets (IS) for a stack of index maps.
 ///
 /// If `map[0] = {0, 1, 2, 3, 4, 5, 6}` and `map[1] = {0, 1, 2, 4}` (in
@@ -100,23 +107,18 @@ Vec create_vector_wrap(const la::Vector<V>& x)
 ///
 /// @param[in] maps Vector of IndexMaps and corresponding block sizes
 /// @param[in] is_bs Requested block sizes for the output vector of PETSc Index Sets
+/// @param[in] ghosted Include ghost indices in the computed IS only,
+///                    if provided the bool true as input.
+/// @param[in] ghost_block_layout Ghost block layout type. IS used for block matrices
+///                               should provide GhostBlockLayout::intertwined.
+///                               IS used for block vectors should provide
+///                               GhostBlockLayout::trailing.
 /// @return Vector of PETSc Index Sets, created on` PETSC_COMM_SELF`
 std::vector<IS> create_index_sets(
     const std::vector<
         std::pair<std::reference_wrapper<const common::IndexMap>, int>>& maps,
-    const std::vector<int> is_bs);
-
-/// Copy blocks from Vec into local vectors
-std::vector<std::vector<PetscScalar>> get_local_vectors(
-    const Vec x,
-    const std::vector<
-        std::pair<std::reference_wrapper<const common::IndexMap>, int>>& maps);
-
-/// Scatter local vectors to Vec
-void scatter_local_vectors(
-    Vec x, const std::vector<std::span<const PetscScalar>>& x_b,
-    const std::vector<
-        std::pair<std::reference_wrapper<const common::IndexMap>, int>>& maps);
+    const std::vector<int> is_bs, bool ghosted = true,
+    GhostBlockLayout ghost_block_layout = GhostBlockLayout::intertwined);
 
 /// Create a PETSc Mat. Caller is responsible for destroying the
 /// returned object.
@@ -235,6 +237,87 @@ public:
 private:
   // PETSc Vec pointer
   Vec _x;
+};
+
+/// Read-only wrapper around a local subvector of a Vec object, used in combination with DofMapRestriction
+class VecSubVectorReadWrapper
+{
+public:
+  /// Constructor (for cases without restriction)
+  VecSubVectorReadWrapper(Vec x,
+                          IS index_set,
+                          bool ghosted = true),
+
+  /// Constructor (for cases with restriction)
+  VecSubVectorReadWrapper(Vec x,
+                          IS unrestricted_index_set,
+                          IS restricted_index_set,
+                          const std::map<std::int32_t, std::int32_t>& unrestricted_to_restricted,
+                          int unrestricted_to_restricted_bs,
+                          bool ghosted = true);
+
+  /// Destructor
+  ~VecSubVectorReadWrapper();
+
+  /// Copy constructor (deleted)
+  VecSubVectorReadWrapper(const VecSubVectorReadWrapper&) = delete;
+
+  /// Move constructor (deleted)
+  VecSubVectorReadWrapper(VecSubVectorReadWrapper&&) = delete;
+
+  // Assignment operator (deleted)
+  VecSubVectorReadWrapper& operator=(const VecSubVectorReadWrapper&) = delete;
+
+  /// Move assignment operator (deleted)
+  VecSubVectorReadWrapper& operator=(VecSubVectorReadWrapper&&) = delete;
+
+  /// Get content
+  std::vector<PetscScalar>& mutable_content() { return _content; }
+
+protected:
+  std::vector<PetscScalar> _content;
+  bool _ghosted;
+};
+
+/// Wrapper around a local subvector of a Vec object, used in combination with DofMapRestriction
+class VecSubVectorWrapper: public VecSubVectorReadWrapper
+{
+public:
+  /// Constructor (for cases without restriction)
+  VecSubVectorWrapper(Vec x,
+                      IS index_set,
+                      bool ghosted = true),
+
+  /// Constructor (for cases with restriction)
+  VecSubVectorWrapper(Vec x,
+                      IS unrestricted_index_set,
+                      IS restricted_index_set,
+                      const std::map<std::int32_t, std::int32_t>& unrestricted_to_restricted,
+                      int unrestricted_to_restricted_bs,
+                      bool ghosted = true);
+
+  /// Destructor
+  ~VecSubVectorWrapper();
+
+  /// Copy constructor (deleted)
+  VecSubVectorWrapper(const VecSubVectorWrapper&) = delete;
+
+  /// Move constructor (deleted)
+  VecSubVectorWrapper(VecSubVectorWrapper&&) = delete;
+
+  // Assignment operator (deleted)
+  VecSubVectorWrapper& operator=(const VecSubVectorWrapper&) = delete;
+
+  /// Move assignment operator (deleted)
+  VecSubVectorWrapper& operator=(VecSubVectorWrapper&&) = delete;
+
+  /// Restore PETSc Vec object
+  void restore();
+
+private:
+  Vec _global_vector;
+  IS _is;
+  std::map<std::int32_t, std::int32_t> _restricted_to_unrestricted;
 };
 
 /// This class is a base class for matrices that can be used in
